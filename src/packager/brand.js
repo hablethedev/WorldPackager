0dbbd606f3502c5657dcfe04dd6b67f8c29281c1
Hable <xendyex@tutanota.com>
2024-06-16 12:44:03 +0100

    Make the thing be the thing so the thing does the thing

--- src/packager/brand.js
+++ src/packager/brand.js
@@ -30,8 +30,8 @@ If this is set to an empty string, the link is removed.
 */
 
 module.exports = {
-  APP_NAME: 'PenguinMod Packager',
-  WEBSITE: 'https://studio.penguinmod.com/PenguinMod-Packager/',
+  APP_NAME: 'World Packager',
+  WEBSITE: 'https://hazel.is-a.dev/WorldPackager/',
   COPYRIGHT_NOTICE: `Copyright (C) 2021-2022 Thomas Weber
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -45,16 +45,14 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.`,
-  ACCENT_COLOR: '#00c3ff',
+  ACCENT_COLOR: '#ff99ff',
   SOURCE_CODE: 'https://github.com/PenguinMod/PenguinMod-Packager',
   FEEDBACK_PRIMARY: {
-    name: 'Discord',
-    link: 'https://discord.gg/NZ9MBMYTZh'
+    name: 'Mail',
+    link: 'mailto:xendyex@tutanota.com'
   },
   FEEDBACK_SECONDARY: {
     name: 'GitHub',
-    link: 'https://github.com/PenguinMod/PenguinMod-Packager/issues'
-  },
-  DONATE: 'https://penguinmod.com/donate',
-  PRIVACY_POLICY: 'https://turbowarp.org/privacy.html',
+    link: 'https://github.com/hablethedev/WorldPackager/issues'
+  }
 };
